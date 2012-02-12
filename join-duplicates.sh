f3aa4c9ad5a9a01dbf19143a0203293eca712e4b
Camilla Berglund <elmindreda@elmindreda.org>
2012-02-12 20:49:20 +0100

    Updated example script to use newer facilities.

--- join-duplicates.sh
+++ join-duplicates.sh
@@ -1,31 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Example script for duff(1).
 #
 # Copyright (c) 2005 Ross Newell
 #
-# Modified Jan 7, 2006 by Camilla Berglund <elmindreda@elmindreda.org>
+# Modified Feb 12, 2012 by Camilla Berglund <elmindreda@elmindreda.org>
 #
 # Uses duff to find duplicate physical files and changes them into hard links
 # to a single physical file, thus saving disk space.  Use with care.
 #
 
-if [ "$1" == '' ]; then
-  echo "usage: `basename $0` directory"
+if [ $# == 0 ]; then
+  echo "Usage: `basename $0` directory [...]"
   exit 1
 fi
 
-duff -r '-f#' -z -p -P "$1" |
+duff -0Dprz -f '%n' "$*" |
 (
-  while read file 
+  count=0
+  while read -d '' -r line 
   do
-    if [ "$file" == '#' ]; then
+    if [ "$count" == 0 ]; then
+      count="$line"
       first=''
     else
       if [ "$first" == '' ]; then
-        first="$file"
+        first="$line"
       else
-	temp=`mktemp -p \`dirname $file\``
+        file="$line"
+	temp="`mktemp -p \`dirname $file\``"
 
 	mv "$file" "$temp" && \
 	ln "$first" "$file" && \
@@ -38,6 +41,7 @@ duff -r '-f#' -z -p -P "$1" |
 	  exit 1
 	fi
       fi
+      count="`expr $count - 1`"
     fi
   done
 )
