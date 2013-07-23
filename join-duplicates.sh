936eed461dade55f19689d89ac78ba30ad945a82
Camilla Berglund <elmindreda@elmindreda.org>
2013-07-23 15:36:34 +0200

    Updated email address.

--- join-duplicates.sh
+++ join-duplicates.sh
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2005 Ross Newell
 #
-# Modified Sep 7, 2012 by Camilla Berglund <elmindreda@elmindreda.org>
+# Modified Sep 7, 2012 by Camilla Berglund <dreda@dreda>
 #
 # Uses duff to find duplicate physical files and changes them into hard links
 # to a single physical file, thus saving disk space.  Use with care.
--- src/duffstring.h
+++ src/duffstring.h
@@ -1,6 +1,6 @@
 /*
  * duff - Duplicate file finder
- * Copyright (c) 2005 Camilla Berglund <elmindreda@elmindreda.org>
+ * Copyright (c) 2005 Camilla Berglund <dreda@dreda.org>
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any
